4ffbc08310f66dcfd3f7e1ba8141efce4d587c23
Ethan Hartman <theeman05@gmail.com>
2026-02-06 21:13:30 -0500

    Only save variables if we have variables lol

--- macro_creator/profile_manager.py
+++ macro_creator/profile_manager.py
@@ -4,12 +4,13 @@ from .variable_config import VariableConfig
 class ProfileManager:
     @staticmethod
     def saveVariables(filepath, variables: dict[str, VariableConfig]):
-        data = {}
-        for key_str, var_config in variables.items():
-            data[key_str] = var_config.toDict()
+        if variables:
+            data = {}
+            for key_str, var_config in variables.items():
+                data[key_str] = var_config.toDict()
 
-        with open(filepath, 'w') as f:
-            json.dump(data, f, indent=4)
+            with open(filepath, 'w') as f:
+                json.dump(data, f, indent=4)
 
     @staticmethod
     def loadVariables(filepath) -> dict[str, VariableConfig]:
